6fd05592a16b4752591604516c23876af79b5d1c
Apurva Shastry <apurva.shastry@gmail.com>
2017-01-18 03:42:04 -0800

    Diner's club, Visa, MasterCard, American Express, Discover and Maestro are identified by the program. Corresponding tests have also been added. (not full coverage though)

--- detectNetwork.js
+++ detectNetwork.js
@@ -13,6 +13,37 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
-};
+  console.log('card_num:' + cardNumber);
+  var length = cardNumber.length;
+  console.log('length = ' + length);
+  var company = '';
+  var prefix = parseInt(cardNumber.slice(0, 1));
+  if (prefix !== 4) {
+  	prefix = parseInt(cardNumber.slice(0, 2));
+  	if (prefix === 50 || prefix === 63 || prefix === 60) {
+  		prefix = parseInt(cardNumber.slice(0, 4));
+  	} else if (prefix === 64) {
+  		prefix = parseInt(cardNumber.slice(0, 6));
+  	}
+  }
+
+  if (length === 14 && (prefix === 38 || prefix === 39)) {
+    company = "Diner's Club";
+  } else if (length === 15 && (prefix === 34 || prefix === 37)) {
+	company = "American Express";
+  } else if (prefix === 4 && (length === 13 || length === 16 || length === 19)) {
+  	company = "Visa";
+  } else if ((prefix >= 51 && prefix <= 55) && length === 16) {
+  	company = "MasterCard";
+  } else if ((prefix === 6011 || prefix === 644649 || prefix === 65) && (length === 16 || length === 19)) {
+  	company = 'Discover';
+  } else if ((prefix === 5018 || prefix === 5020 || prefix === 5038 || prefix === 6304) && (length >= 12 && length <= 19)) {
+  	company = 'Maestro';
+  }
+
+  console.log(company);
+  return company;
+
+}
 
 
